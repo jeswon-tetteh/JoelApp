6a89e0f70bdb1f57edb882c3b6b6548ec323859b
Joel Jeswon Tetteh <jjtetteh001@st.ug.edu.gh>
2024-07-05 13:53:40 +0000

    Changed background color to light green

--- JoelApp/App.js
+++ JoelApp/App.js
@@ -16,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#808080",
+        backgroundColor: "#90EE90",
     alignItems: "center",
     justifyContent: "center",
   },
